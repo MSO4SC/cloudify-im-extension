f193e6787f479bcb544173cbd3708d7a4cccf24f
victorsndvg <victorsv@gmail.com>
2018-11-14 08:31:11 +0100

    test non-script plugin

---
+++ plugin/nodes/flavour.py
@@ -0,0 +1,73 @@
+from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
+from cloudify.decorators import operation
+from cloudify.exceptions import *
+
+def reset_log_indentation():
+    ctx.instance.runtime_properties['indent'] = 0
+
+def get_log_indentation():
+    if 'indent' in ctx.instance.runtime_properties:
+        return '\t'*ctx.instance.runtime_properties['indent']
+    else:
+        return ''
+
+def increase_log_indentation():
+    ctx.instance.runtime_properties['indent'] += 1
+
+def decrease_log_indentation():
+    ctx.instance.runtime_properties['indent'] -= 1
+
+def get_child(dictionary, key, required=False, debug=False):
+    child = None
+    if key not in dictionary:
+        msg = '{0} Required key "{1}" not defined!'.format(get_log_indentation(), str(key))
+        if required:
+            raise NonRecoverableError(msg)
+        else:
+            ctx.logger.debug(msg)
+    else:
+        ctx.logger.debug('{0} Obtaining key "{1}"'.format(get_log_indentation(), str(key)))
+        child = dictionary[key]
+    return child
+
+def create_child(dictionary, key, value):
+    child = get_child(dictionary=dictionary, key=key, required=False)
+    if child is None:
+        msg = '{0} New key "{1}" defined!'.format(get_log_indentation(), str(key))
+        ctx.logger.debug(msg)
+        dictionary[key] = value
+        child = dictionary[key]
+    return child
+
+
+def build_radl_flavour():
+    ctx.logger.debug('{0} Infrastructure Manager deployment info:'.format(get_log_indentation()))
+    increase_log_indentation()
+
+    config   = get_child(dictionary=inputs, key='config', required=True)
+    type = get_child(dictionary=config, key='type', required=True)
+    cores = get_child(dictionary=config, key='cores', required=True)
+    memory = get_child(dictionary=config, key='memory', required=True)
+
+    flavour_radl = \
+"    instance_type = '" + str(type) + "' and \n" + \
+"    cpu.count = " + str(cores) + " and \n" + \
+"    memory.size = " + str(memory) + " and \n"
+
+    decrease_log_indentation()
+    return flavour_radl
+
+@operation
+def configure():
+    if (not get_child(dictionary=inputs, key='simulate', required=False)):
+        reset_log_indentation()
+        ctx.logger.debug('{0} Configure operation: Begin'.format(get_log_indentation()))
+        increase_log_indentation()
+        radl = get_child(ctx.instance.runtime_properties, key='settings')
+        if not radl:
+            radl = create_child(ctx.instance.runtime_properties, key='settings', value={})
+        radl_network = create_child(radl, key='flavour', value=build_radl_flavour())
+        decrease_log_indentation()
+        ctx.logger.debug('{0} Configure operation: End'.format(get_log_indentation()))
+    
