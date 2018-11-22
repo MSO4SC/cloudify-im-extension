95222fd574cb649815851b04656360d10e774986
victorsndvg <victorsv@gmail.com>
2018-11-22 13:17:14 +0100

    Isolate keypar in a new node. Preparation   - New keypair node   - Asynchronous start operation while waiting for configuration

--- plugin/nodes/keypair.py
+++ plugin/nodes/keypair.py
@@ -4,23 +4,19 @@ from cloudify.decorators import operation
 from cloudify.exceptions import *
 from plugin.nodes.utils import *
 
-def build_radl_image(config):
+def build_radl_keypair(config):
     ctx.logger.debug('{0} Infrastructure Manager deployment info:'.format(get_log_indentation()))
     increase_log_indentation()
 
-    id = get_child(dictionary=config, key='id', required=True)
-    storage = get_child(dictionary=config, key='storage')
-    username = get_child(dictionary=config, key='username', required=True)
-    password = get_child(dictionary=config, key='password', required=True)
+    public_key = get_child(dictionary=config, key='public_key', required=True)
+    private_key = get_child(dictionary=config, key='private_key', required=True)
 
-    image_radl = ''
-    image_radl += "    disk.0.image.url = '" + str(id) + "' and \n"
-    if storage: image_radl += "    disk.0.image.size = '" + str(storage) + "' and \n" 
-    image_radl += "    disk.0.os.credentials.username = '" + str(username) + "' and \n"
-    image_radl += "    disk.0.os.credentials.password = '" + str(password) + "' and \n"
+    keypair_radl = ''
+    keypair_radl += "    disk.0.os.credentials.public_key = '" + str(public_key.strip()) + "' and \n"
+    keypair_radl += "    disk.0.os.credentials.private_key = '" + str(private_key.strip()) + "' "
 
     decrease_log_indentation()
-    return image_radl
+    return keypair_radl
 
 @operation
 def configure(config, simulate, **kwargs):
@@ -31,7 +27,7 @@ def configure(config, simulate, **kwargs):
         radl = get_child(ctx.instance.runtime_properties, key='settings')
         if not radl:
             radl = create_child(ctx.instance.runtime_properties, key='settings', value={})
-        radl_image = create_child(radl, key='image', value=build_radl_image(config))
+        radl_image = create_child(radl, key='keypair', value=build_radl_keypair(config))
         decrease_log_indentation()
         ctx.logger.debug('{0} Configure operation: End'.format(get_log_indentation()))
 
